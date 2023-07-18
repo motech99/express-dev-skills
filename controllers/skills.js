c7f46bf835a305be1dc3bafd3a19523bf5c99896
Mohammed <mhani9@icloud.com>
2023-07-18 15:22:06 +1000

    finished controllers module

---
+++ controllers/skills.js
@@ -0,0 +1,20 @@
+const skills = [
+    {id: 1, skill: 'Task breakdown', accquired: true },
+    {id: 2, skill: 'Problem-solving', accquired: false },
+    {id: 3, skill: 'Thoughtful designer', accquired: true},
+    {id: 4, skill: 'Continuous learning', accquired: true}
+];
+
+module.exports = {
+    getAll,
+    getOne
+};
+
+function getAll() {
+    return skills;
+}
+
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find(skill => skill.id === id);
+}
