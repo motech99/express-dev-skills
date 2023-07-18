ae829fd5133eeff891c39be19f2364b67a082099
Mohammed <mhani9@icloud.com>
2023-07-18 15:30:26 +1000

    finished controllers module

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,20 +1,18 @@
-const skills = [
-    {id: 1, skill: 'Task breakdown', accquired: true },
-    {id: 2, skill: 'Problem-solving', accquired: false },
-    {id: 3, skill: 'Thoughtful designer', accquired: true},
-    {id: 4, skill: 'Continuous learning', accquired: true}
-];
+const skill = require("../models/skill");
 
 module.exports = {
-    getAll,
-    getOne
+  index,
+  show,
 };
 
-function getAll() {
-    return skills;
+function index(req, res) {
+  res.render("skills/index", {
+    skills: Skill.getAll(),
+  });
 }
 
-function getOne(id) {
-    id = parseInt(id);
-    return skills.find(skill => skill.id === id);
+function show(req, res) {
+  res.render("skills/show", {
+    skills: Skill.getOne(req.params.id),
+  });
 }
